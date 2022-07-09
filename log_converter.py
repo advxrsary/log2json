82934cce8c4ef7c0fcbbdef8ee18dc035469b8d5
ghost <sevastian.zare@knf.stud.vu.lt>
2022-07-09 18:27:47 +0300

    rename first/second_dict to dict1/2

--- log_converter.py
+++ log_converter.py
@@ -1,13 +1,17 @@
 import json
+from itertools import islice
 
 file_name = 'email.txt'
 json_file = 'email.json'
 file = open(file_name, "r")
 jfile = open(json_file, "w")
 data = []
-order = ["time", "sessionid", "message"]
 message_list = []
 new_message_list = {}
+final_dict = {}
+
+def seq_pairs(li):
+    return zip(li, islice(li, 1, None))
 
 def process_data():
     for line in file.readlines():
@@ -18,10 +22,18 @@ def process_data():
         structure = {key: value for key, value in zip(
             ["time", "sessionid", message_details[0]], details)}
         data.append(structure)
-    print(data)
+    return data
         
 def create_cell():
-    pass
+    for dict1, dict2 in seq_pairs(process_data()):
+        # if first_dict['sessionid'] == second_dict['sessionid']:
+        #     second_dict.pop('time')
+        #     final_dict = first_dict | second_dict
+        #     print(final_dict)
+        dict2_sorted = {i: dict2[i] for i in dict1.keys()}
+        keys = dict1.keys()
+        values = zip(dict1.values(), dict2_sorted.values())
+        final_dict = dict(zip(keys, values))
 
 def write_json():
     jfile.write('[\n')
@@ -31,6 +43,5 @@ def write_json():
     jfile.write(']')
 
 if __name__ == "__main__":
-    process_data()
+    create_cell()
     write_json()
-    get_messages()
