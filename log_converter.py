5f6e13354971039af00ac9b725de89bb92a11810
ghost <sevastian.zare@knf.stud.vu.lt>
2022-07-09 15:57:45 +0300

    divided messages output; tryna make cells

--- log_converter.py
+++ log_converter.py
@@ -1,4 +1,5 @@
 import json
+import re
 
 file_name = 'email.txt'
 json_file = 'email.json'
@@ -6,15 +7,37 @@ file = open(file_name, "r")
 jfile = open(json_file, "w")
 data = []
 order = ["time", "sessionid", "message"]
-newlist = []
+message_list = []
 
-def create_json_data():
+
+
+
+def process_data():
     for line in file.readlines():
         details = line.split("\t") 
         details = [x.strip() for x in details]
-        newlist.append(details[2].split('=')[1])
+        message_list.append(details[2])
+        
+                
         structure = {key:value for key, value in zip(order, details)}
         data.append(structure)
+        
+def get_messages():
+    for item in message_list:
+        if 'client=' in item:
+            client_list = {'client': (item.replace('client=', ''))}
+        if 'from=' in item:
+            message_from = {'from': (item.replace('from=', ''))}
+        if 'to=' in item:
+            message_to = {'to': (item.replace('to=', ''))}
+        if 'message-id=' in item:
+            message_id = {'messageid': (item.replace('message-id=', ''))}
+        if 'status=' in item:
+            status = {'status': (item.replace('status=', ''))}
+            
+
+def create_cell():
+    pass
 
 def write_json():
     jfile.write('[\n')
@@ -24,6 +47,7 @@ def write_json():
     jfile.write(']')
 
 if __name__ == "__main__":
-    create_json_data()
+    process_data()
     write_json()
-    print(sorted(newlist))
+    get_messages()
+    # print(sorted(outputlist))
