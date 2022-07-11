7033e517c75914564ba9306e18b05ff58882e420
ghost <sevastian.zare@knf.stud.vu.lt>
2022-07-11 05:58:55 +0300

    👌🏻 Bug fixes, cleanup, methods segmentation, comments

--- log2json.py
+++ log2json.py
@@ -1,5 +1,4 @@
 import json, sys
-from fix_comma import fix_comma
 from itertools import islice, groupby
 from datetime import datetime
 from termcolor import colored
@@ -9,37 +8,17 @@ time_val, session_val, start_val, dur_val, from_val, to_val, status_val, client_
 data, sidlist, datelist = [], [], []
 time = {"start": start_val, "duration": dur_val}
 address = {"from": from_val, "to": to_val}
-date_format = '%Y-%m-%d %H:%M:%S.%f'
 
-# Exceptions for files input
-try:
-    file_name = sys.argv[1]
-except IndexError:
-    print(colored('\n[#]', 'red'), colored("Specify log file!"))
-    print(colored('[#]', 'red'), colored("Specify output file!"))
-    sys.exit(1)
-try:
-    json_file = sys.argv[2]
-except IndexError:
-    print(colored('\n[#]', 'red'), colored("Specify output file!"))
-    sys.exit(1)
-
-# Sort by sessionid key
-def key_func(k):
-    return k['sessionid']
-
+# Divide one list into two, the second is ahead of the first 
+# by one step
 def seq_pairs(li):
     return zip(li, islice(li, 1, None))
 
-# Data processor, takes each line of the log,
-# from each line takes values from each row
-# from the third row takes the part before sign
-# of equality as a key and after as a value for this key.
-# zips all values and creates list of dictionaries.
-# returns the list
-def process_data():
+# Processes each row; from the third row takes <key>=<value>
+# returns a datalist
+def process_data(file2process):
     print(colored('\n[*]', 'yellow'), "Processing data...")
-    file = open(file_name, "r")
+    file = open(file2process, "r")
     for line in file.readlines():
         details = line.split("\t")
         details = [x.strip() for x in details]
@@ -51,8 +30,9 @@ def process_data():
     return data
 
 # Calculates duration of a single event
-def calculate_duration():
-    data_old = seq_pairs(process_data())
+def calc_duration(data2calc):
+    date_format = '%Y-%m-%d %H:%M:%S.%f'
+    data_old = seq_pairs(data2calc)
     new_data = []
     for list1, list2 in data_old:
         start = datetime.strptime(list1['start'].replace("T", ' '), date_format)
@@ -63,29 +43,36 @@ def calculate_duration():
     return(new_data)
 
 
-# Converts log to json
-def create_event():
+# Sorts and groups the datalist by session id
+def sort_by_sid(data2sort):
+    sorted_data = sorted(data2sort, key=key_func)
+    for key, value in groupby(sorted_data, key_func):
+        sidlist.append(list(value))
+    return sidlist
+# Return sessionid val from dict
+def key_func(k):
+    return k['sessionid']
+    
+
+# Transforms processed data into serializable format
+# and writes it to .json file
+def write_file(file):
+    processed_data = process_data(file) 
+    sorted_data = sort_by_sid(calc_duration(processed_data))
     event = {"time": time,
-        	"sessionid": session_val,
-            "client": client_val,
-            "messageid": messageid_val,
-            "address": address,
-            "status": status_val}
-    jwfile = open(json_file, "w")
+             "sessionid": session_val,
+             "client": client_val,
+             "messageid": messageid_val,
+             "address": address,
+             "status": status_val}
     etime = event['time']
     eadrs = event['address']
-    datalist = calculate_duration()
-    datalist = sorted(datalist, key=key_func)
     print(colored('[*]', 'yellow'), "Creating event...")
 
-    # Sorts and groups the datalist by session id
-    for key, value in groupby(datalist, key_func):
-        sidlist.append(list(value))
-
-    # Transforms processed data into serializable format
-    # and writes it to .json file
+    # Write the file
+    jwfile = open(json_file, "w")
     jwfile.write("[")
-    for item in sidlist:
+    for item in sorted_data:
         jwfile.write('\n')
         for i in item:
             event['sessionid'] = i['sessionid']
@@ -103,10 +90,26 @@ def create_event():
                 event['messageid'] = i['message-id']
         jwfile.write(f"{json.dumps(event, indent=4)},")
     jwfile.write("]")
-    
-# Main func
+
+# Self-execution
 if __name__ == "__main__":
-    create_event()
+    # Exception
+    try:
+        file_name = sys.argv[1]
+    except IndexError:
+        print(colored('\n[#]', 'red'), "Specify log file!")
+        print(colored('[#]', 'red'), "Specify output file!")
+        sys.exit(1)
+    try:
+        json_file = sys.argv[2]
+    except IndexError:
+        print(colored('\n[#]', 'red'), "Specify output file!")
+        sys.exit(1)
+
+    # Main func
+    write_file(file_name)
+
+    # Fancy output
     print(colored('[+]', 'green'), "Done!")
     print(colored('[+]', 'green'), f"Results: {json_file}")
     print(colored('[:]', 'grey'),
