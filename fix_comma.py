05362df9c52f610edaed11b7e7e3e8cfedc5b9b2
ghost <sevastian.zare@knf.stud.vu.lt>
2022-07-11 06:01:25 +0300

    👌🏻 Method segmentation

--- fix_comma.py
+++ fix_comma.py
@@ -1,13 +1,6 @@
 import sys
 from termcolor import colored
 
-# Chech if file is specified
-try:
-    file_name = sys.argv[1]
-except IndexError:
-    print(colored('\n[#]', 'red'), colored("Specify log file!"))
-    sys.exit(1)
-
 # Read json, search for pattern and replace comma with \n
 def fix_comma(thefile):
     print(colored('[*]', 'yellow'), "Fixing comma...")
@@ -16,4 +9,15 @@ def fix_comma(thefile):
     x = text.replace('},]', '}\n]')
     with open(thefile, "w") as file:
         file.writelines(x)
-    print(colored('[+]', 'green'), "File fixed!")
+    print(colored('[+]', 'green'), f"File {thefile} fixed!")
+
+if __name__ == "__main__":
+    # Check if file is specified
+    try:
+        file_name = sys.argv[1]
+    except IndexError:
+        print(colored('\n[#]', 'red'), "Specify log file!")
+        sys.exit(1)
+
+    # Self-execute
+    fix_comma(file_name)
