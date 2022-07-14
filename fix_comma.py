31aaad546eb27e846fae80e04f6892936f0a8e10
ghost <sevastian.zare@knf.stud.vu.lt>
2022-07-14 12:06:27 +0300

    👌🏻 write_file() func

--- fix_comma.py
+++ fix_comma.py
@@ -9,11 +9,12 @@ def fix_comma(thefile):
     read_file = open(thefile, "r")
     text = read_file.read()
     x = text.replace('},]', '}\n]')
-    with open(thefile, "w") as file:
-        file.writelines(x)
-    print(colored('[+]', 'green'), f"File {thefile} fixed!")
     return x
-        
+    
+def write_file(thefile, text):
+    with open(thefile, "w") as file:
+        file.writelines(text)
+    print(colored('[+]', 'green'), f"File {thefile} fixed!")
     
 if __name__ == "__main__":
     # Check if file is specified
@@ -24,4 +25,5 @@ if __name__ == "__main__":
         sys.exit(1)
 
     # Self-execute
-    fix_comma(file_name)
+    x = fix_comma(file_name)
+    write_file(file_name, x)
