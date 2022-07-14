fb4433976c469e78c3492a38dba6baf33aeb4781
s <sevastian.zare@knf.stud.vu.lt>
2022-07-14 15:23:59 +0300

    👌🏻 fancy back
    * ✍🏻 Update README.md
    
    * 👌🏻 some fancy stuff

--- fix_comma.py
+++ fix_comma.py
@@ -1,21 +1,24 @@
 import sys
 from termcolor import colored
 
-
 # Read json, search for pattern and replace comma with \n
+
+
 def fix_comma(thefile):
-    print(colored('[*]', 'yellow'), "Fixing comma...")
+    print(colored('\n[‡]', 'blue'), "Fixing comma...")
     raw_out = []
     read_file = open(thefile, "r")
     text = read_file.read()
     x = text.replace('},]', '}\n]')
     return x
-    
+
+
 def write_file(thefile, text):
     with open(thefile, "w") as file:
         file.writelines(text)
-    print(colored('[+]', 'green'), f"File {thefile} fixed!")
-    
+    print(colored('[‡]', 'blue'), f"File {thefile} fixed!")
+
+
 if __name__ == "__main__":
     # Check if file is specified
     try:
--- log2json.py
+++ log2json.py
@@ -7,26 +7,31 @@ __description__ = """ Processes data from a log file and converts it to
                       json serialized file. """
 
 
-import json, sys
+import json
+import sys
 from itertools import islice, groupby
 from datetime import datetime
 from termcolor import colored
 from fix_comma import write_file as write_file_fix
 from fix_comma import fix_comma
 
-# Global variables declaration 
+# Global variables declaration
 time_val, session_val, start_val, dur_val, from_val, to_val, status_val, client_val, messageid_val = "", "", "", "", "", "", "", "", ""
 data, sidlist, datelist = [], [], []
 time = {"start": start_val, "duration": dur_val}
 address = {"from": from_val, "to": to_val}
 
-# Divide one list into two, the second is ahead of the first 
+# Divide one list into two, the second is ahead of the first
 # by one step
+
+
 def seq_pairs(li):
     return zip(li, islice(li, 1, None))
 
 # Processes each row; from the third row takes <key>=<value>
 # returns a datalist
+
+
 def process_data(file2process):
     print(colored('\n[*]', 'yellow'), "Processing data...")
     file = open(file2process, "r")
@@ -47,7 +52,8 @@ def calc_duration(data2calc):
     data_old = seq_pairs(data2calc)
     new_data = []
     for list1, list2 in data_old:
-        start = datetime.strptime(list1['start'].replace("T", ' '), date_format)
+        start = datetime.strptime(
+            list1['start'].replace("T", ' '), date_format)
         end = datetime.strptime(list2['start'].replace("T", ' '), date_format)
         duration = str(end - start)
         list1['duration'] = duration
@@ -62,13 +68,17 @@ def sort_by_sid(data2sort):
         sidlist.append(list(value))
     return sidlist
 # Return sessionid val from dict
+
+
 def key_func(k):
     return k['sessionid']
 
 # Transforms processed data into serializable format
 # and writes it to .json file
+
+
 def write_file(file):
-    processed_data = process_data(file) 
+    processed_data = process_data(file)
     sorted_data = sort_by_sid(calc_duration(processed_data))
     event = {"time": time,
              "sessionid": session_val,
@@ -101,17 +111,20 @@ def write_file(file):
                 event['messageid'] = i['message-id']
         jwfile.write(f"{json.dumps(event, indent=4)},")
     jwfile.write("]")
+    print(colored('[*]', 'yellow'), "Writing to file...")
 
 
 # Self-execution
 if __name__ == "__main__":
-    # Exception
-    print('''  _             ____   _                 
+    # Banner
+    print(f'''  _             ____   _                 
  | | ___   __ _|___ \ (_)___  ___  _ __  
  | |/ _ \ / _` | __) || / __|/ _ \| '_ \ 
  | | (_) | (_| |/ __/ | \__ \ (_) | | | |
  |_|\___/ \__, |_____|/ |___/\___/|_| |_|
-          |___/     |__/     by advxrs4ry''')
+          |___/     |__/    {colored('by advxrsary','green')}''')
+
+    # Exception
     try:
         file_name = sys.argv[1]
     except IndexError:
@@ -126,11 +139,10 @@ if __name__ == "__main__":
 
     # Main func
     write_file(file_name)
-    x = fix_comma(json_file)
-    print(colored('[+]', 'green'), "Done!")
-    write_file_fix(json_file, x)
     print(colored('[+]', 'green'), f"Results: {json_file}")
-    
+    x = fix_comma(json_file)
+    write_file_fix(json_file, x)
+
     # print(colored('[:]', 'white'),
     #       colored('To fix the comma run:', 'white'))
     # print(colored('[:]', 'white'),
